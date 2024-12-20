eee3074dfe63aa2612686413ed29bbedd95ba7cc
Akin Karadag <akin.karadag@outlook.com>
2024-12-20 22:06:32 +0100

    Anpassung Testklasse SecurityConfigTest

--- src/test/java/Config/SecurityConfigTest.java
+++ src/test/java/Config/SecurityConfigTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -46,19 +47,13 @@ public class SecurityConfigTest {
     @Autowired
     private WebApplicationContext context;
 
-    @MockBean
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-    @MockBean
+    @Autowired
     private JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    @MockBean
+    @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
-    @MockBean
-    private UserDetailsServiceImpl userDetailsService;
-
-    @MockBean
+    @Autowired
     private AuthenticationManager authenticationManager;
 
     private MockMvc mockMvc;
